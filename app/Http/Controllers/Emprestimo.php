c8220ac47fe55ad932546b9c5af1f5938ef4e4e0
gustavo <gustavoalechandre002@gmail.com>
2024-01-02 15:46:06 -0300

    up

--- app/Http/Controllers/Emprestimo.php
+++ app/Http/Controllers/Emprestimo.php
@@ -48,7 +48,7 @@ class Emprestimo extends Controller
 
     public function Ranking()
     {
-        $rankingStudents = Emprestimos::select('idDoAluno', DB::raw('COUNT(*) as total'))
+        $rankingStudents = Emprestimos::select('idDoAluno, created_at, update_at', DB::raw('COUNT(*) as total'))
             ->groupBy('idDoAluno')
             ->orderByDesc('total')
             ->get();
